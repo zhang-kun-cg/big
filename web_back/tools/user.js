d183568f6c3d4044a3073cc481274fc0dc210be8
zhangkun <905027145@qq.com>
2019-12-15 17:12:44 +0800

    登陆与退出

--- web_back/tools/user.js
+++ web_back/tools/user.js
@@ -1,11 +1,32 @@
-function dl(data, obj) {
-    $.ajax({
-        type: 'post',
-        url: LOGIN,
-        data: {
-            user_name: data.a,
-            password: data.b
-        },
-        success: obj
-    })
+var use = {
+    dl: function (obj) {
+        $.ajax({
+            type: 'post',
+            url: LOGIN,
+            data: {
+                user_name: obj.data.a,
+                password: obj.data.b
+            },
+            success: function (res) {
+                if (res.code === 200) {
+                    obj.success();
+                } else {
+                    obj.fail();
+                }
+            }
+        })
+    },
+    tc: function (obj) {
+        $.ajax({
+            type: 'post',
+            url: LOGOUT,
+            success: function (res) {
+                if (res.code === 200) {
+                    obj.success();
+                } else {
+                    obj.fail();
+                }
+            }
+        })
+    }
 }
